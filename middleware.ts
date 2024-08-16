bc6c48cab9adbda8faf2df6564e591e6558b6d5e
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2024-08-16 20:33:21 +1000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-const isProtectedRoute = createRouteMatcher([
-    '/bookings(.*)',
-    '/checkout(.*)',
-    '/favorites(.*)',
-    '/profile(.*)',
-    '/properties(.*)',
-    '/rentals(.*)',
-    '/reviews(.*)',
-]);
 
-export default clerkMiddleware((auth, req) =>{
-    if(isProtectedRoute(req)) auth().protect()
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher(['/', '/properties(.*)']);
+
+const isAdminRoute = createRouteMatcher(['/admin(.*)']);
+export default clerkMiddleware(async (auth, req) => {
+  const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+  if (!isPublicRoute(req)) auth().protect();
 });
+
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-  };
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
