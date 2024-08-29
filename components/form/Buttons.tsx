224a6d119e36bb7ec2460f6feda84d37ecdfb889
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2024-08-29 22:52:19 +1000

    fixed error on form buttons

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,78 +1,105 @@
-'use client'
-import { ReloadIcon } from "@radix-ui/react-icons";
-import {useFormStatus } from 'react-dom';
-import {Button} from "@/components/ui/button";
+'use client';
+
+import { ReloadIcon } from '@radix-ui/react-icons';
+import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
 import { SignInButton } from '@clerk/nextjs';
 import { FaRegHeart, FaHeart } from 'react-icons/fa';
-import { LuTrash2, LuPenSquare } from "react-icons/lu";
-
-
+import { LuTrash2, LuPenSquare } from 'react-icons/lu';
 type btnSize = 'default' | 'lg' | 'sm';
 
 type SubmitButtonProps = {
   className?: string;
-  text?:string;
-  size?:btnSize;
+  text?: string;
+  size?: btnSize;
 };
 
-export const SubmitButton = ({
-  className='', 
-  text='submit', 
-  size='lg',
-}:SubmitButtonProps) => {
-  const {pending} =useFormStatus();
+export function SubmitButton({
+  className = '',
+  text = 'submit',
+  size = 'lg',
+}: SubmitButtonProps) {
+  const { pending } = useFormStatus();
+
   return (
-   <Button type='submit' disabled={pending} className={`capitalize ${className}`} size={size}>
-    {pending ? <>
-    <ReloadIcon className="mr-2 h-4 w-4 animate-spin"/>
-    Please wait...
-    </> : text}
-   </Button>
-  );
-};
-
-export const CardSignInButton = () =>{
-  return(<SignInButton mode='modal'>
-    <Button type='button' size='icon' variant="outline" className="p-2 cursor-pointer" asChild>
-      <FaRegHeart />
+    <Button
+      type='submit'
+      disabled={pending}
+      className={`capitalize ${className} `}
+      size={size}
+    >
+      {pending ? (
+        <>
+          <ReloadIcon className='mr-2 h-4 w-4 animate-spin' />
+          Please wait...
+        </>
+      ) : (
+        text
+      )}
     </Button>
-  </SignInButton>
+  );
+}
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
+      <Button
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
   );
 };
 
-export const CardSubmitButton = ({isFavorite}: {isFavorite:boolean}) =>{
-const {pending} = useFormStatus();
-return 
-<Button type='submit' size='icon' variant='outline' className="p-2 cursor-pointer"> 
-{pending ? (
-<ReloadIcon className="animage-spin"/> 
-) : isFavorite? (
-<FaHeart />
-) : (<FaRegHeart />
-
-)}
-</Button>
+export const CardSubmitButton = ({ isFavorite }: { isFavorite: boolean }) => {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='outline'
+      className='p-2 cursor-pointer'
+    >
+      {pending ? (
+        <ReloadIcon className='animate-spin' />
+      ) : isFavorite ? (
+        <FaHeart />
+      ) : (
+        <FaRegHeart />
+      )}
+    </Button>
+  );
 };
-
 
 type actionType = 'edit' | 'delete';
+export const IconButton = ({ actionType }: { actionType: actionType }) => {
+  const { pending } = useFormStatus();
 
-export const IconButton = ({ actionType }: { actionType:actionType }) => {
-  const {pending} = useFormStatus();
-  const renderIcon = () =>{
-switch(actionType){
-  case 'edit':
-      return <LuPenSquare />;
-  case 'delete':
-       return <LuTrash2 />; 
-  default:
-    const never: never = actionType;
-    throw new Error(`Invalid action type: ${never}`);
-};
-};
-return (
-<Button type='submit' size='icon' variant="link" className="p-2 cursor-pointer">
-{pending ? <ReloadIcon className="animate-spin"/> :renderIcon()}
-</Button>
-);
+  const renderIcon = () => {
+    switch (actionType) {
+      case 'edit':
+        return <LuPenSquare />;
+      case 'delete':
+        return <LuTrash2 />;
+      default:
+        const never: never = actionType;
+        throw new Error(`Invalid action type: ${never}`);
+    }
+  };
+
+  return (
+    <Button
+      type='submit'
+      size='icon'
+      variant='link'
+      className='p-2 cursor-pointer'
+    >
+      {pending ? <ReloadIcon className=' animate-spin' /> : renderIcon()}
+    </Button>
+  );
 };
